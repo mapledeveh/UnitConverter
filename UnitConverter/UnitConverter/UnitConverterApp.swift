8ccee5033257e0dd2e846d0585fb80949440e7aa
mapledeveh <mapledeveh@gmail.com>
2023-08-12 22:17:02 -0400

    Rename Challenge 1 to UnitConverter

---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Alex Nguyen on 2023-04-30.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
